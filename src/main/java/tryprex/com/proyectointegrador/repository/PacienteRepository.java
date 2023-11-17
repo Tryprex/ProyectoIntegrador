512b460dc13b18a56039af2033ef1edb1961c4ef
Karlaa28 <karlacamino2017.28@gmail.com>
2023-11-17 07:51:22 -0500

    errores

--- src/main/java/tryprex/com/proyectointegrador/repository/PacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package tryprex.com.proyectointegrador.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tryprex.com.proyectointegrador.model.bd.Paciente;
-
-@Repository
-public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
-}
